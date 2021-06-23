31011a4eb7de0697a38de50e26d257c6a059da59
P1SC3S03 <catarina-m-sousa@hotmail.com>
2021-06-23 14:44:50 +0100

    Added Packages and Classes & Changed Location of txt files
    Added/Cahgned Command, Server and Player Classes
    Changed location of easy.txt and hard.txt

---
+++ src/Util/Timer.java
@@ -0,0 +1,46 @@
+package Util;
+
+public class Timer {
+
+    int minutes;
+    int seconds;
+
+    public Timer (int minutes){
+        this.minutes =  minutes-1;
+    }
+
+    public void timer() {
+
+        if (minutes < 0){
+            return;
+        }
+
+        System.out.print("Time   ");
+
+        while (true) {
+            seconds--;
+            if (seconds == -1) {
+                seconds = 59;
+            }
+
+            System.out.print(minutes+":"+seconds);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("\b\b\b\b"); //VERIFY
+
+            if (minutes == 0 && seconds == 0) {
+                System.out.println("Time is over!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Timer timer = new Timer(1);
+        timer.timer();
+    }
+
+}
+
