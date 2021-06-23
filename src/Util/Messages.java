e631219a2e55a29a7ed16d4df2f5a17bc67a5e04
P1SC3S03 <catarina-m-sousa@hotmail.com>
2021-06-23 11:57:03 +0100

    Added txt and changed Messages.java
    Added the questions txt files [easy, hard]
    Changed Messages.java to include images

--- src/Util/Messages.java
+++ src/Util/Messages.java
@@ -1,4 +1,29 @@
 package Util;
 
 public class Messages {
+
+    public static final String HOOKED = "  " +
+            "          FISHKISSFISHKIS               \n" +
+            "       SFISHKISSFISHKISSFISH            F\n" +
+            "    ISHK   ISSFISHKISSFISHKISS         FI\n" +
+            "  SHKISS   FISHKISSFISHKISSFISS       FIS\n" +
+            "HKISSFISHKISSFISHKISSFISHKISSFISH    KISS\n" +
+            "  FISHKISSFISHKISSFISHKISSFISHKISS  FISHK\n" +
+            "      SSFISHKISSFISHKISSFISHKISSFISHKISSF\n" +
+            "  ISHKISSFISHKISSFISHKISSFISHKISSF  ISHKI\n" +
+            "SSFISHKISSFISHKISSFISHKISSFISHKIS    SFIS\n" +
+            "  HKISSFISHKISSFISHKISSFISHKISS       FIS\n" +
+            "    HKISSFISHKISSFISHKISSFISHK         IS\n" +
+            "       SFISHKISSFISHKISSFISH            K\n" +
+            "         ISSFISHKISSFISHK               ";
+
+
+    public static final String SLEEPING_SERVER = "" +
+            "   ______ \n" +
+            "  /     /| \n" +
+            " /     /#|---(ZZZZZZzzzzZZZZZzzzzzzzzz)\n" +
+            "/_____/##|\n"+
+            "|     |##/\n"+
+            "|     |#/\n"+
+            "|_____|/ \n";
 }
